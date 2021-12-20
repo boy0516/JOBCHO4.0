abc8a1f3bd17aee83fd7edc9553572a14cdedb24
choi <boy0516@naver.com>
2021-12-20 11:34:18 +0900

    new jobcho

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "user-service is running";
+    }
+
+}
+
